0694f24e31a8a4774e033ceaa7c2b23bcfb8646a
Andrew Charneski <acharneski@gmail.com>
2023-12-03 13:16:13 -0500

    http client config

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     `java-library`
     `maven-publish`
-    id("org.jetbrains.kotlin.jvm") version "1.9.20"
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
     id("signing")
 }
 
