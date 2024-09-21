e7108bba8f577e7c3a7bd43e7da83599f4f6fee0
Andrew Charneski <acharneski@gmail.com>
2024-09-21 14:49:39 -0400

    typescript describer

---
+++ src/main/kotlin/com/simiacryptus/jopenai/describe/AbbrevWhitelistTSDescriber.kt
@@ -0,0 +1,21 @@
+package com.simiacryptus.jopenai.describe
+
+import com.simiacryptus.jopenai.describe.DescriptorUtil.componentType
+import com.simiacryptus.jopenai.describe.DescriptorUtil.isArray
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+
+open class AbbrevWhitelistTSDescriber(private vararg val abbreviated: String) : TypeScriptDescriber() {
+    override fun isAbbreviated(self: Type): Boolean {
+        if (self.typeName in primitives) {
+            return false
+        } else if (self is ParameterizedType && List::class.java.isAssignableFrom(self.rawType as Class<*>)) {
+            return isAbbreviated(self.actualTypeArguments[0])
+        } else if (self is ParameterizedType && Map::class.java.isAssignableFrom(self.rawType as Class<*>)) {
+            return isAbbreviated(self.actualTypeArguments[0]) && isAbbreviated(self.actualTypeArguments[1])
+        } else if (self.isArray) {
+            return isAbbreviated(self.componentType!!)
+        }
+        return (abbreviated.find { self.typeName.startsWith(it) } == null) || super.isAbbreviated(self)
+    }
+}
