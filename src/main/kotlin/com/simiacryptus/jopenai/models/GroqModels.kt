e79a18c9d57eca3e5ec921549e336132f9e5bf41
Andrew Charneski <acharneski@gmail.com>
2025-04-06 13:17:16 -0400

    wip

--- src/main/kotlin/com/simiacryptus/jopenai/models/GroqModels.kt
+++ src/main/kotlin/com/simiacryptus/jopenai/models/GroqModels.kt
@@ -4,24 +4,14 @@ import org.slf4j.LoggerFactory
 object GroqModels {
     private val logger = LoggerFactory.getLogger(GroqModels::class.java)
 
-
     val Llama33_70bVersatile = ChatModel(
         name = "Llama33_70bVersatile",
         modelName = "llama-3.3-70b-versatile",
         maxTotalTokens = 128000,
-        maxOutTokens = 32768,
+        maxOutTokens = 32768, 
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.59,
         outputTokenPricePerK = 0.79
-    )
-    val Mixtral8x7bInstructV01 = ChatModel(
-        name = "Mixtral8x7bInstructV01",
-        modelName = "mixtral-8x7b-32768",
-        maxTotalTokens = 32768,
-        maxOutTokens = 32768,
-        provider = APIProvider.Groq,
-        inputTokenPricePerK = 0.24,
-        outputTokenPricePerK = 0.24
     )
 
     val Gemma2_9b = ChatModel(
@@ -34,7 +24,6 @@ object GroqModels {
         outputTokenPricePerK = 0.20
     )
 
-
     val Llama33_70bSpecDec = ChatModel(
         name = "Llama33_70bSpecDec",
         modelName = "llama-3.3-70b-specdec",
@@ -44,6 +33,7 @@ object GroqModels {
         inputTokenPricePerK = 0.59,
         outputTokenPricePerK = 0.99
     )
+    
     val Llama31_8bInstant = ChatModel(
         name = "Llama31_8bInstant",
         modelName = "llama-3.1-8b-instant",
@@ -53,6 +43,7 @@ object GroqModels {
         inputTokenPricePerK = 0.05,
         outputTokenPricePerK = 0.08
     )
+    
     val Llama32_1bPreview = ChatModel(
         name = "Llama32_1bPreview",
         modelName = "llama-3.2-1b-preview",
@@ -62,6 +53,7 @@ object GroqModels {
         inputTokenPricePerK = 0.04,
         outputTokenPricePerK = 0.04
     )
+    
     val Llama32_3bPreview = ChatModel(
         name = "Llama32_3bPreview",
         modelName = "llama-3.2-3b-preview",
@@ -71,6 +63,7 @@ object GroqModels {
         inputTokenPricePerK = 0.06,
         outputTokenPricePerK = 0.06
     )
+    
     val LlamaGuard38b = ChatModel(
         name = "LlamaGuard38b",
         modelName = "llama-guard-3-8b",
@@ -80,6 +73,7 @@ object GroqModels {
         inputTokenPricePerK = 0.20,
         outputTokenPricePerK = 0.20
     )
+    
     val Llama370b8192 = ChatModel(
         name = "Llama370b8192",
         modelName = "llama3-70b-8192",
@@ -89,6 +83,7 @@ object GroqModels {
         inputTokenPricePerK = 0.59,
         outputTokenPricePerK = 0.79
     )
+    
     val Llama38b8192 = ChatModel(
         name = "Llama38b8192",
         modelName = "llama3-8b-8192",
@@ -98,75 +93,87 @@ object GroqModels {
         inputTokenPricePerK = 0.05,
         outputTokenPricePerK = 0.08
     )
-    val Mixtral8x7b32768 = ChatModel(
-        name = "Mixtral8x7b32768",
-        modelName = "mixtral-8x7b-32768",
-        maxTotalTokens = 32768,
-        maxOutTokens = 32768,
-        provider = APIProvider.Groq,
-        inputTokenPricePerK = 0.24,
-        outputTokenPricePerK = 0.24
-    )
+    
     val Qwen25_32b = ChatModel(
         name = "Qwen25_32b",
         modelName = "qwen-2.5-32b",
-        maxTotalTokens = 32768,
-        maxOutTokens = 32768,
+        maxTotalTokens = 128000,
+        maxOutTokens = 16384,
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.30,
         outputTokenPricePerK = 0.30
     )
+    val Qwen25Coder32b = ChatModel(
+        name = "Qwen25Coder32b",
+        modelName = "qwen-2.5-coder-32b",
+        maxTotalTokens = 128000,
+        maxOutTokens = 16384,
+        provider = APIProvider.Groq,
+        inputTokenPricePerK = 0.30,
+        outputTokenPricePerK = 0.30
+    )
+    val QwenQwq32b = ChatModel(
+        name = "QwenQwq32b",
+        modelName = "qwen-qwq-32b",
+        maxTotalTokens = 128000,
+        maxOutTokens = 16384,
+        provider = APIProvider.Groq,
+        inputTokenPricePerK = 0.30,
+        outputTokenPricePerK = 0.30
+    )
+    val MistralSaba24b = ChatModel(
+        name = "MistralSaba24b",
+        modelName = "mistral-saba-24b",
+        maxTotalTokens = 32000,
+        maxOutTokens = 16384,
+        provider = APIProvider.Groq,
+        inputTokenPricePerK = 0.25,
+        outputTokenPricePerK = 0.25
+    )
+    
     val DeepseekQwen32b = ChatModel(
         name = "DeepseekQwen32b",
         modelName = "deepseek-r1-distill-qwen-32b",
-        maxTotalTokens = 32768,
-        maxOutTokens = 32768,
+        maxTotalTokens = 128000,
+        maxOutTokens = 16384,
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.30,
         outputTokenPricePerK = 0.30
     )
-    val DeepseekLlama70bSpecDec = ChatModel(
-        name = "DeepseekLlama70bSpecDec",
-        modelName = "deepseek-r1-distill-llama-70b-specdec",
-        maxTotalTokens = 8192,
-        maxOutTokens = 8192,
-        provider = APIProvider.Groq,
-        inputTokenPricePerK = 0.59,
-        outputTokenPricePerK = 0.99
-    )
+    
     val DeepseekLlama70b = ChatModel(
         name = "DeepseekLlama70b",
         modelName = "deepseek-r1-distill-llama-70b",
-        maxTotalTokens = 32768,
-        maxOutTokens = 32768,
+        maxTotalTokens = 128000,
+        maxOutTokens = 16384,
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.59,
         outputTokenPricePerK = 0.79
     )
+    
     val Llama32_11bVision = ChatModel(
         name = "Llama32_11bVision",
         modelName = "llama-3.2-11b-vision-preview",
-        maxTotalTokens = 8192,
+        maxTotalTokens = 128000,
         maxOutTokens = 8192,
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.10,
         outputTokenPricePerK = 0.10
     )
+    
     val Llama32_90bVision = ChatModel(
         name = "Llama32_90bVision",
         modelName = "llama-3.2-90b-vision-preview",
-        maxTotalTokens = 8192,
+        maxTotalTokens = 128000,
         maxOutTokens = 8192,
         provider = APIProvider.Groq,
         inputTokenPricePerK = 0.70,
         outputTokenPricePerK = 0.70
     )
 
-
     val values = mapOf(
         "Llama33_70bVersatile" to Llama33_70bVersatile,
         "Gemma2_9b" to Gemma2_9b,
-        "Mixtral8x7bInstructV01" to Mixtral8x7bInstructV01,
         "Llama31_8bInstant" to Llama31_8bInstant,
         "Llama32_1bPreview" to Llama32_1bPreview,
         "Llama32_3bPreview" to Llama32_3bPreview,
@@ -174,13 +181,13 @@ object GroqModels {
         "LlamaGuard38b" to LlamaGuard38b,
         "Llama370b8192" to Llama370b8192,
         "Llama38b8192" to Llama38b8192,
-        "Mixtral8x7b32768" to Mixtral8x7b32768,
         "Qwen25_32b" to Qwen25_32b,
+        "Qwen25Coder32b" to Qwen25Coder32b,
+        "QwenQwq32b" to QwenQwq32b,
+        "MistralSaba24b" to MistralSaba24b,
         "DeepseekQwen32b" to DeepseekQwen32b,
-        "DeepseekLlama70bSpecDec" to DeepseekLlama70bSpecDec,
         "DeepseekLlama70b" to DeepseekLlama70b,
         "Llama32_11bVision" to Llama32_11bVision,
         "Llama32_90bVision" to Llama32_90bVision
     )
-
 }
